61318e37e04ac9e5afd58b754b20ec913ce025d2
Pranav <pranav.bhawan@gmail.com>
2020-09-08 09:55:12 +0530

    Added Capstone Project from Py4E

---
+++ Coursera Py4E Capstone/README.md
@@ -0,0 +1,4 @@
+# coursera-capstone-python-py4e
+Code for Capstone: Retrieving, Processing, and Visualizing Data with Python, university of michigan
+
+Contains code for py4e course, taught by Dr. Charles Severance
---
+++ Coursera Py4E Capstone/gmane/gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Month','umich.edu','ucdavis.edu','ufp.pt','mac.com','berkeley.edu','uct.ac.za','columbia.edu','indiana.edu','unl.edu','mtu.edu'],
+['2005-12',57,11,10,12,12,14,13,12,12,11],
+['2006-01',3,6,6,3,3,0,0,1,0,0]
+];
